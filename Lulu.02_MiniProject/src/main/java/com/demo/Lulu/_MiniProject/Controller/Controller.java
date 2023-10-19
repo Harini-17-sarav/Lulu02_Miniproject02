edcd30c97ef89d3ceb7e241fd27abbbac2da625b
Harini-17-sarav <harinisarav17@gmail.com>
2023-10-19 16:11:31 +0530

    first commit

---
+++ Lulu.02_MiniProject/src/main/java/com/demo/Lulu/_MiniProject/Controller/Controller.java
@@ -0,0 +1,87 @@
+package com.demo.Lulu._MiniProject.Controller;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.demo.Lulu._MiniProject.Entity.Clothes;
+import com.demo.Lulu._MiniProject.Repository.ClothesRepository;
+
+@RestController
+
+@RequestMapping("/pps")
+public class Controller {
+	
+	@Autowired
+
+	 
+
+	ClothesRepository clothesRepository;
+
+
+
+	@GetMapping("/getProduct")
+
+	public List<Clothes> getJPQL() {
+
+		return clothesRepository.findAll(); // select * from <TableName>;
+
+	}
+
+
+
+	
+
+	@PostMapping("/addProduct")
+
+	public Clothes createuser(@RequestBody Clothes prod) {
+
+		return clothesRepository.save(prod);
+
+	}
+
+
+
+	
+
+	@DeleteMapping("/delProduct/{id}")
+
+	public void delProduct(@PathVariable("id")Integer id) {
+
+		clothesRepository.deleteById(id);
+
+	}
+
+
+
+	
+
+	@PutMapping("/UpdateProduct/{id}")
+
+	public ResponseEntity<Object> updateShipping(@RequestBody Clothes clothes, @PathVariable long id) {
+
+Optional<Clothes> clothesOptional = clothesRepository.findById((int) id);
+
+	if (clothesOptional.isEmpty())
+
+	return ResponseEntity.notFound().build();
+
+	clothes.setId(id);
+
+	clothesRepository.save(clothes);
+
+	return ResponseEntity.noContent().build();
+
+
+
+}
+
+}
---
+++ Lulu.02_MiniProject/src/main/java/com/demo/Lulu/_MiniProject/Repository/ClothesRepository.java
@@ -0,0 +1,11 @@
+package com.demo.Lulu._MiniProject.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.demo.Lulu._MiniProject.Entity.Clothes;
+
+	public interface ClothesRepository extends JpaRepository <Clothes, Integer> {
+
+	}
+
+	 
+
